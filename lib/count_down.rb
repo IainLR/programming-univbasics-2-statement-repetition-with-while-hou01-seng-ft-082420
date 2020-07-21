9c76b093d1bab0d901857a75d76d759aacf4094f
Iain Robertson <iainrobertzon@gmail.com>
2020-07-21 22:22:09 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,9 @@
 # Write your code here
-exit_number = 8 
-count = 0 
+exit_number = 0 
+count = 10
 while count < 10 do 
   break if count == exit_number 
   puts "the number #{count}, yay"
-  count += 1 
+  count -= 1 
 end
+puts "Happy New Year"
